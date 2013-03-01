1c95d28e8d1939a538489c7bd642de6f86343530
Kevin C <kkevinchou@gmail.com>
2013-02-28 22:32:33 -0500

    rpcInit()

--- rpcserver.cpp
+++ rpcserver.cpp
@@ -1,7 +1,14 @@
 #include "rpcserver.hpp"
-
+#include "helpers.h"
 void rpcInit() {
+    char * binderAddressString = getenv ("BINDER_ADDRESS");
+    char * binderPortString = getenv("BINDER_PORT");
 
+    if(binderAddressString == NULL) error("ERROR: SERVER_ADDRESS environment variable not set.");
+    if(binderPortString == NULL) error("ERROR: SERVER_PORT environment variable not set.");
+
+    int binderSocketFd = setupSocketAndReturnDescriptor(binderAddressString, binderPortString);
+    int localSocketFd = createSocket();
 }
 
 int rpcRegister(char *name, int *argTypes, skeleton f) {
--- server.cpp
+++ server.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-
+rpcInit();
 
 
 
